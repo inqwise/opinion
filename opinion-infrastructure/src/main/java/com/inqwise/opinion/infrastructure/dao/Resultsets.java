9ef7753bf67c2e541eaf1c80856ad3cf3c9f7ae7
terra <tera.g@inqwise.com>
2024-01-09 08:58:05 +0200

    make method/ function result set

--- opinion-infrastructure/src/main/java/com/inqwise/opinion/infrastructure/dao/Resultsets.java
+++ opinion-infrastructure/src/main/java/com/inqwise/opinion/infrastructure/dao/Resultsets.java
@@ -1,5 +1,35 @@
 package com.inqwise.opinion.infrastructure.dao;
 
-public class Resultsets {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.List;
+
+import org.jooq.impl.DSL;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ResultSets {
 	
+	public static JSONArray parse(Connection connection, ResultSet resultSet){
+		
+		
+		List<JSONObject> list = DSL.using(connection)
+		.fetch(resultSet)
+		.map(r -> {
+			JSONObject obj = new JSONObject();
+			
+			for(var field : r.fields()) {
+				obj.put(field.getName(), r.getValue(field));
+			}
+			return obj;
+		});
+		
+		return new JSONArray(list);
+		
+	}
+	
+	
+	//JSONArray result = ResultSets.parse(connection, resultSet);
+
+
 }
